5df58b300ae15044bfcc6fa3e4b1c2aab4f15ae1
kevindoveton <kevindoveton@me.com>
2017-02-01 10:24:51 +1030

    inline css with html using juice

---
+++ src/inlineify.js
@@ -0,0 +1,10 @@
+var juice = require('juice');
+
+var options = {
+	extraCss: "li {font-size: 2em} h1 {font-size: .5em} h2 {font-size 1em}"
+}
+
+
+module.exports = function(html, css) {
+	return juice(html, options)
+}
