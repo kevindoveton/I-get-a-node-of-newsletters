e8b1c7a6f307785e0b92aa70d00826bb24ba132f
kevindoveton <kevindoveton@me.com>
2017-02-03 21:14:07 +1030

    updated sass test

--- test/test-sass.js
+++ test/test-sass.js
@@ -4,11 +4,16 @@ const assert = require('assert');
 describe('sass', function() {
 	
 	it('should return valid css', function() {
+		// setup
 		const fs = require('fs');
 		fs.writeFileSync('test.sass', 'h1\n  font-size: 3em');
+		
+		// actual work
 		var result = require('../src/sass')('test.sass');
 		var expected = 'h1{font-size:3em}\n'
 		assert.equal(expected, result);
+		
+		// packup
 		fs.unlink('test.sass');
 	});
 	
